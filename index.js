19d6cd0be7b372a2a4cc82c03de697c00dfcbd1d
Feozard <feozard@gmail.com>
2023-01-07 13:16:33 +0100

    add list interactions

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var isOnClients = true;
+var isOnClients = true; // true if we are on clients tab, false if we are on orders tab
 
-document.getElementById("searchBar").addEventListener("keypress", function(event) {
+document.getElementById("searchBar").addEventListener("keypress", function(event) { // search when pressing enter
     if (event.key === "Enter") {
         search();
       }
 });
 
-function search() {
+function search() { // check in which tab we are
     if (isOnClients) {
         searchClients();
     }
@@ -15,49 +15,92 @@ function search() {
     }
 }
 
+document.getElementById("clients").addEventListener("click", switchClients);    // switch to clients tab
+document.getElementById("orders").addEventListener("click", switchOrders);  // switch to orders tab
 
 // Clients
 
-document.getElementById("clients").addEventListener("click", switchClients);
-document.getElementById("orders").addEventListener("click", switchOrders);
+document.getElementById("addTitle").addEventListener("click", addClient);
 
 function switchClients() {
-    isOnClients = true;
-    document.getElementById("clients").style.borderBottom = "4px solid #22333B";
-    document.getElementById("orders").style.borderBottom = "4px solid transparent";
+    if (!isOnClients) {
+        document.getElementById("clients").style.borderBottom = "4px solid #22333B";    
+        document.getElementById("orders").style.borderBottom = "4px solid transparent";
 
-    document.getElementById("searchTitle").innerHTML = "<img src='Icons/search.svg' alt='search icon' class='icon'> Rechercher un client";
-    document.getElementById("listTitle").innerHTML = "<img src='Icons/list.svg' alt='list icon' class='icon'> Liste des clients";
+        // change titles
+        document.getElementById("searchTitle").innerHTML = "<img src='Icons/search.svg' alt='search icon' class='icon'> Rechercher un client";
+        document.getElementById("listTitle").innerHTML = "<img src='Icons/list.svg' alt='list icon' class='icon'> Liste des clients";
+        document.getElementById("addTitle").innerHTML = "<img src='Icons/add.svg' alt='add icon' class='icon'> Ajouter un client";
 
+        // add interactions
+        document.getElementById("addTitle").removeEventListener("click", addOrder);
+        document.getElementById("addTitle").addEventListener("click", addClient);
 
-    document.getElementById("searchBar").value = "";
+        // remove export button
+        var listBox = document.getElementById("listBox");
+        var exportBtn = document.getElementById("exportBtn");
+        listBox.removeChild(exportBtn);
+
+        document.getElementById("searchBar").value = "";    // clear searchbar
+
+        isOnClients = true;
+    }
 }
 
 function searchClients() {
-    let search = document.getElementById("searchBar");
+    let search = document.getElementById("searchBar");  // get search value
     let searchValue = search.value;
     console.log("search in clients");
     console.log(searchValue);
 }
 
+function addClient() {
+    console.log("add client");
+}
 
 
 // Orders
 
 function switchOrders() {
-    isOnClients = false;
-    document.getElementById("clients").style.borderBottom = "4px solid transparent";
-    document.getElementById("orders").style.borderBottom = "4px solid #22333B";
+    if (isOnClients) {
+        document.getElementById("clients").style.borderBottom = "4px solid transparent";
+        document.getElementById("orders").style.borderBottom = "4px solid #22333B";
+    
+        // change titles
+        document.getElementById("searchTitle").innerHTML = "<img src='Icons/search.svg' alt='search icon' class='icon'> Rechercher une commande";
+        document.getElementById("listTitle").innerHTML = "<img src='Icons/list.svg' alt='list icon' class='icon'> Liste des commandes";
+        document.getElementById("addTitle").innerHTML = "<img src='Icons/add.svg' alt='add icon' class='icon'> Ajouter une commande";
+    
+        // add export button
+        var listBox = document.getElementById("listBox");
+        var exportBtn = document.createElement("p");
+        exportBtn.innerHTML = "<img src='Icons/export.svg' alt='export icon' class='icon'> Exporter";
+        exportBtn.id = "exportBtn";
+        exportBtn.className = "titleSection";
+        listBox.appendChild(exportBtn);
 
-    document.getElementById("searchTitle").innerHTML = "<img src='Icons/search.svg' alt='search icon' class='icon'> Rechercher une commande";
-    document.getElementById("listTitle").innerHTML = "<img src='Icons/list.svg' alt='list icon' class='icon'> Liste des commandes";
-
-    document.getElementById("searchBar").value = "";
+        // add interactions
+        document.getElementById("addTitle").removeEventListener("click", addClient);
+        document.getElementById("addTitle").addEventListener("click", addOrder);
+        document.getElementById("exportBtn").addEventListener("click", exportList);
+    
+        document.getElementById("searchBar").value = "";    // clear searchbar
+    
+        isOnClients = false;
+    }
 }
 
 function searchOrders() {
-    let search = document.getElementById("searchBar");
+    let search = document.getElementById("searchBar");  // get search value
     let searchValue = search.value;
     console.log("search in orders");
     console.log(searchValue);
+}
+
+function addOrder() {
+    console.log("add order");
+}
+
+function exportList() {
+    console.log("export list");
 }
